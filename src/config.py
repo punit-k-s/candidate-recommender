7d5c08316ba89281edc162b00ce9a7b8642d2d9f
punit-k-s <pshet@asu.edu>
2025-08-08 16:43:08 -0700

    Update config

--- src/config.py
+++ src/config.py
@@ -5,4 +5,4 @@ load_dotenv()  # loads .env if present
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 EMBED_MODEL = os.getenv("EMBED_MODEL", "text-embedding-3-small")
-TOP_K_DEFAULT = 10
+TOP_K_DEFAULT = 5
